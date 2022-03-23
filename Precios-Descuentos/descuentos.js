2830447e4188704acc7a903de2dc89e89f6ad923
Santiago Morales <Santi@Morales.com>
2022-03-23 11:03:21 -0300

    Segundo taller, falta css 2

---
+++ Precios-Descuentos/descuentos.js
@@ -0,0 +1,47 @@
+// const precioOriginal = 120;
+// const descuento = 18;
+
+function calcularPrecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+  
+    return precioConDescuento;
+}
+
+coupon = ["Basic", "Expert", "Expert+"];
+  
+function onClickButtonPriceDiscount() {
+  const inputPrice = document.getElementById('inputPrice');
+  const valuePrice = inputPrice.value;
+  const selectCoupons = document.getElementById('inputCoupons');
+  const valueCoupons = selectCoupons.value;
+
+  let discount;
+
+  switch (valueCoupons) {
+      case '0': 
+          discount = 15;
+          break;
+      case '1':
+          discount = 25;
+          break;
+      case '2':
+          discount = 35;
+          break;
+  }
+
+  const precioConDescuento = calcularPrecioConDescuento(valuePrice, discount);
+  const resultP = document.getElementById('resultP');
+  //resultP.style.display = 'block';
+  resultP.innerText = '$' + precioConDescuento;
+}
+
+
+
+
+// console.log({
+//     precioOriginal,
+//     descuento,
+//     porcentajePrecioConDescuento,
+//     precioConDescuento
+// });
